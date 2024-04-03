3cd902c1b3fe7f0d1b1cd94509e9a74cf96df45d
EKTA <ektamangal8076@gmail.com>
2024-04-03 17:06:01 +0530

    Updates

---
+++ precache-manifest.8f86cc8e2c289c5edb36db481c799dde.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d448bbe8f629bff873b8a99bd533d2f4",
+    "url": "/TextUtils---React/index.html"
+  },
+  {
+    "revision": "796140e54dea025830a4",
+    "url": "/TextUtils---React/static/css/main.435d5fe5.chunk.css"
+  },
+  {
+    "revision": "e84e5d26e2865de0408d",
+    "url": "/TextUtils---React/static/js/2.1e1764c1.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/TextUtils---React/static/js/2.1e1764c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "054847683c20e1268d65",
+    "url": "/TextUtils---React/static/js/3.fef4592a.chunk.js"
+  },
+  {
+    "revision": "796140e54dea025830a4",
+    "url": "/TextUtils---React/static/js/main.1aca8bbf.chunk.js"
+  },
+  {
+    "revision": "a482ba51729ecdf06f72",
+    "url": "/TextUtils---React/static/js/runtime-main.71aabedf.js"
+  }
+]);
